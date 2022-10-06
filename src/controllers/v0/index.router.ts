1cafbb6bb81e4bbd1b3d6c02bbd818d46c7ac90b
kmanadkat <kmanadkat@gmail.com>
2022-10-06 21:30:21 +0530

    chore: Prettier Code Formatting

--- src/controllers/v0/index.router.ts
+++ src/controllers/v0/index.router.ts
@@ -1,14 +1,14 @@
-import { Router, Request, Response } from 'express';
-import { FeedRouter } from './feed/routes/feed.router';
-import { UserRouter } from './users/routes/user.router';
+import { Router, Request, Response } from 'express'
+import { FeedRouter } from './feed/routes/feed.router'
+import { UserRouter } from './users/routes/user.router'
 
-const router: Router = Router();
+const router: Router = Router()
 
-router.use('/feed', FeedRouter);
-router.use('/users', UserRouter);
+router.use('/feed', FeedRouter)
+router.use('/users', UserRouter)
 
-router.get('/', async (req: Request, res: Response) => {    
-    res.send(`V0`);
-});
+router.get('/', async (req: Request, res: Response) => {
+	res.send(`V0`)
+})
 
-export const IndexRouter: Router = router;
+export const IndexRouter: Router = router
