7636fbe4248a624642a58224fc28d1671cdf423a
kmanadkat <kmanadkat@gmail.com>
2022-10-08 10:25:46 +0530

    feat: Bcrypt Password Hashing & Compare Fns Added

--- src/controllers/v0/users/routes/auth.router.ts
+++ src/controllers/v0/users/routes/auth.router.ts
@@ -12,16 +12,17 @@ import { config } from '../../../../config/config'
 const router: Router = Router()
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-	//@TODO Use Bcrypt to Generated Salted Hashed Passwords
-	return 'NotYetImplemented'
+	const saltRounds = 10
+	const salt = await bcrypt.genSalt(saltRounds)
+	const hash = await bcrypt.hash(plainTextPassword, salt)
+	return hash
 }
 
 async function comparePasswords(
 	plainTextPassword: string,
 	hash: string
 ): Promise<boolean> {
-	//@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-	return true
+	return await bcrypt.compare(plainTextPassword, hash)
 }
 
 function generateJWT(user: User): string {
